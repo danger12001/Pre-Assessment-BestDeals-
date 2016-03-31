b8f62e6d556b09707add2dd7daa7af820aeb0290
danger12001 <paul@projectcodex.co>
2016-03-31 12:47:30 +0200

    Cheapest Orange Seller Complete

--- fruition.js
+++ fruition.js
@@ -1,32 +1,42 @@
-exports.JSON = function(){
-var JSON = [{
-  "woolingsworth": {
-    "apples": 4,
-    "bananas": 3,
-    "oranges": 12
-  },
-
-  "chockers": {
-    "bananas": 2,
-    "apples": 5,
-    "oranges": 4
-  },
-
-  "pickle pay": {
-    "bananas": 4,
-    "oranges": 7
-  },
-
-  "shopwrong": {
-    "apples": 2,
-    "bananas": 3
-  },
-
-  "kwakspar": {
-    "oranges": 9,
-    "apples": 4
+exports.JSONd = function() {
+  var JSONd = {
+    "woolingsworth": {
+      "apples": 4,
+      "bananas": 3,
+      "oranges": 12
+    },
+    "chockers": {
+      "bananas": 2,
+      "apples": 5,
+      "oranges": 4
+    },
+    "picklepay": {
+      "bananas": 4,
+      "oranges": 7
+    },
+    "shopwrong": {
+      "apples": 2,
+      "bananas": 3
+    },
+    "kwakspar": {
+      "oranges": 9,
+      "apples": 4
+    }
+  };
+  return JSONd;
+};
+exports.cOrangeSeller = function(JSONd) {
+  var orangeSellers = [];
+  for (var shop in JSONd) {
+    if (JSONd[shop].hasOwnProperty("oranges")) {
+      orangeSellers.push({
+        'shop': shop,
+        'price': JSONd[shop].oranges
+      });
+    }
   }
-}];
-// console.log(JSON);
-return JSON;
+  var cheapestSeller = orangeSellers.sort(function(a,b){
+    return a.price - b.price;
+  });
+  return cheapestSeller[0].shop;
 };
--- test/fruition_test.js
+++ test/fruition_test.js
@@ -1,13 +1,60 @@
 var assert = require('assert');
 var fruition = require('../fruition');
-describe("JSON",function(){
-  it("should return the JSON maps",function(){
-    var result = fruition.JSON();
-    assert.deepEqual(result,[{ woolingsworth: { apples: 4, bananas: 3, oranges: 12 },
-  chockers: { bananas: 2, apples: 5, oranges: 4 },
-  'pickle pay': { bananas: 4, oranges: 7 },
-  shopwrong: { apples: 2, bananas: 3 },
-  kwakspar: { oranges: 9, apples: 4 } }]
-);
+describe("JSONd", function() {
+  it("should return the JSON maps", function() {
+    var result = fruition.JSONd();
+    assert.deepEqual(result, {
+      woolingsworth: {
+        apples: 4,
+        bananas: 3,
+        oranges: 12
+      },
+      chockers: {
+        bananas: 2,
+        apples: 5,
+        oranges: 4
+      },
+      picklepay: {
+        bananas: 4,
+        oranges: 7
+      },
+      shopwrong: {
+        apples: 2,
+        bananas: 3
+      },
+      kwakspar: {
+        oranges: 9,
+        apples: 4
+      }
+    });
+  });
 });
+describe("cOrangeSeller", function() {
+  it("should return the SELLER of the cheapest ORANGES", function() {
+    var result = fruition.cOrangeSeller({
+      woolingsworth: {
+        apples: 4,
+        bananas: 3,
+        oranges: 12
+      },
+      chockers: {
+        bananas: 2,
+        apples: 5,
+        oranges: 4
+      },
+      picklepay: {
+        bananas: 4,
+        oranges: 7
+      },
+      shopwrong: {
+        apples: 2,
+        bananas: 3
+      },
+      kwakspar: {
+        oranges: 9,
+        apples: 4
+      }
+    });
+    assert.equal(result, "chockers");
+  });
 });
